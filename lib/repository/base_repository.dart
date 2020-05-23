cc8020961107d0b244e95bd516ca165ba3955df6
Frederick-S <mao_xiaodan@hotmail.com>
2020-05-23 14:19:02 +0800

    refactor: extract method

--- lib/repository/base_repository.dart
+++ lib/repository/base_repository.dart
@@ -7,26 +7,24 @@ class BaseRepository {
     Future<Database> database = openDatabase(
       join(databasePath, 'nhk-easy.db'),
       onCreate: (db, version) {
-        final batch = db.batch();
-        batch.execute(
-            'CREATE TABLE IF NOT EXISTS config(id INTEGER PRIMARY KEY, newsFetchedStartUtc TEXT, newsFetchedEndUtc TEXT)');
-        batch.execute(
-            'CREATE TABLE IF NOT EXISTS news(newsId TEXT PRIMARY KEY, title TEXT, titleWithRuby TEXT, body TEXT, imageUrl TEXT, publishedAtUtc TEXT, publishedAtEpoch INTEGER)');
-
-        return batch.commit();
+        _executeScript(db);
       },
       onUpgrade: (db, oldVersion, newVersion) {
-        final batch = db.batch();
-        batch.execute(
-            'CREATE TABLE IF NOT EXISTS config(id INTEGER PRIMARY KEY, newsFetchedStartUtc TEXT, newsFetchedEndUtc TEXT)');
-        batch.execute(
-            'CREATE TABLE IF NOT EXISTS news(newsId TEXT PRIMARY KEY, title TEXT, titleWithRuby TEXT, body TEXT, imageUrl TEXT, publishedAtUtc TEXT)');
-
-        return batch.commit();
+        _executeScript(db);
       },
       version: 3,
     );
 
     return database;
+  }
+
+  void _executeScript(Database db) {
+    final batch = db.batch();
+    batch.execute(
+        'CREATE TABLE IF NOT EXISTS config(id INTEGER PRIMARY KEY, newsFetchedStartUtc TEXT, newsFetchedEndUtc TEXT)');
+    batch.execute(
+        'CREATE TABLE IF NOT EXISTS news(newsId TEXT PRIMARY KEY, title TEXT, titleWithRuby TEXT, body TEXT, imageUrl TEXT, publishedAtUtc TEXT)');
+
+    batch.commit();
   }
 }
